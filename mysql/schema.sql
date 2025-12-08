1736b217fd4fd418c815fa499075c9423d941816
siva <sivaram0434@gmail.com>
2025-12-08 11:59:48 +0530

    add the schema

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -4,7 +4,8 @@ USE crud_app;
 CREATE TABLE entries (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   amount DOUBLE NOT NULL,
-  description VARCHAR(255) NOT NULL
+  description VARCHAR(255) NOT NULL,
+  date DATE NOT NULL  
 );
 
 CREATE USER IF NOT EXISTS 'crud'@'%' IDENTIFIED BY 'CrudApp@1';
