0a56d5f4a38509f4f2f76c73ec4aa566c8ad11fc
siva <sivaram0434@gmail.com>
2025-12-08 14:54:41 +0530

    error was removed

--- crud-frontend/src/components/EntriesTable.tsx
+++ crud-frontend/src/components/EntriesTable.tsx
@@ -151,14 +151,14 @@ const EntriesTable: React.FC<EntriesTableProps> = ({ entries = [], loading, onEn
                       />
                       </td>
                     <td className="border border-[#1b1b2f] p-3 text-base text-left text-white">
-                      {!enableUpdate ? `${entry.date}` : 
+                      !enableUpdate ? {entry.date} : 
                       <input
                         type="string"
                         id="date"
                         value={entry.date}
                         onChange={(e) => setDate(e.target.value)}
                         className="my-3 p-3.5 w-full text-base border border-[#162447] rounded bg-[#1b1b2f] text-white focus:border-[#1f78ff] focus:outline-none"
-                      />}
+                      />
                     </td>
                     }
                     <td className="border border-[#1b1b2f] p-3 text-base text-left text-white">
